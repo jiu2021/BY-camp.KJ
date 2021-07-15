4109d507e8afe718292455742c5475921f4bc9ed
KJ <1358940365@qq.com>
2021-07-15 12:31:48 +0800

    轮播图

--- summer-camp-2021/KJ/Tasks/Task for CSS-jianshu/jianshu.js
+++ summer-camp-2021/KJ/Tasks/Task for CSS-jianshu/jianshu.js
@@ -1,52 +1,35 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlidesAuto(); //自动播放
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function movetimer() {
+    clearInterval(timer); //清除定时器
 }
 
-//function currentSlide(n) {
-//  showSlides(slideIndex = n);
-//}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    //var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    //for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
-    slides[slideIndex - 1].style.display = "block";
-    // dots[slideIndex-1].className += " active";
+function timerplay() {
+    showSlidesAuto(); //恢复定时器
 }
-/*var slideIndex = 0;
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
-function showSlides() {
+function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    //var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 0 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    //dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000);
-}*/
+}
+
+function showSlidesAuto() {
+    timer = setInterval(function() {
+        plusSlides(1);
+    }, 2000);
+}
