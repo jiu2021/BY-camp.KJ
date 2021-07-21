b08b8c9fefbd7eadfc3b5be7fdbab508802819d7
KJ <1358940365@qq.com>
2021-07-21 20:06:18 +0800

    day17

--- summer-camp-2021/KJ/Tasks/Task for nodejs/app.js
+++ summer-camp-2021/KJ/Tasks/Task for nodejs/app.js
@@ -1,5 +1,5 @@
-'use strict'
-/*const http = require('http');
+/*'use strict'
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const common = require('./common');
@@ -9,7 +9,7 @@ http.createServer(
         let pathname = req.url;
         let extname = path.extname(pathname);
         let mime = common.getMime(extname);
-        fs.readFile('./catchme' + pathname, function(err, data) {
+        fs.readFile('./Task for todolist' + pathname, function(err, data) {
             if (err) {
                 res.writeHead(404, { 'Content-Type': '' + mime + ';charset=utf-8' });
                 res.end("404 页面不存在");
@@ -19,16 +19,17 @@ http.createServer(
             }
         });
     }
-).listen(3002);
-console.log("Listening...");*/
+).listen(9521);
+console.log("http://127.0.0.1:9521 Listening...");*/
 
 //导入所依赖的库
 const http = require("http");
 const url = require("url");
 const path = require("path");
-const mime = require("mime");
 const fs = require("fs");
 const urlencode = require('urlencode');
+const common = require('./common');
+//const mime = require("mime");
 
 //服务器框架
 http.createServer(function(req, res) {
@@ -59,7 +60,7 @@ http.createServer(function(req, res) {
                         //创建超链接打开文件
                         for (var i in files) {
                             var filename = files[i];
-                            html += "<div><p>|-<a href='http://127.0.0.1:9999" +
+                            html += "<div><p>|-<a href='http://127.0.0.1:9521" +
                                 pathname + '/' + filename + "'>" + filename + "</a></p></div>";
                         }
                         res.writeHead(200, { 'content-type': 'text/html' });
@@ -70,10 +71,13 @@ http.createServer(function(req, res) {
             //如果是文件
             else if (fs.statSync(finalpath).isFile()) {
                 //打开文件
+                let extname = path.extname(pathname);
+                let mime = common.getMime(extname);
                 fs.readFile(finalpath, function(err, data) {
                     if (err) res.end("cannot read file!");
                     else {
-                        res.writeHead(200, { "Content-Type": mime.getType(path.basename) });
+                        res.writeHead(200, { 'Content-Type': '' + mime + ';charset=utf-8' });
+                        //res.writeHead(200, { "Content-Type": mime.getType(path.basename(pathname)) });
                         res.end(data);
                     }
                 })
@@ -83,5 +87,5 @@ http.createServer(function(req, res) {
             }
         }
     })
-}).listen(9999); //设置监听端口
-console.log("Listening...");
+}).listen(9521); //设置监听端口
+console.log("http://127.0.0.1:9521 Listening...");
